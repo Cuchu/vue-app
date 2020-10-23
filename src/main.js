2acb4f5e78c39bc377a7f4628706f665d752bad6
Erwin Maximiliano Schvindt <maximiliano.schvindt@wellsky.com>
2020-10-22 23:21:30 -0300

    Bootstrap and axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+import Axios from "axios";
+import VueAxios from "vue-axios";
+
 import App from "./components/App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import "./assets/style.scss";
+
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(VueAxios, Axios);
 
 new Vue({
   router,
