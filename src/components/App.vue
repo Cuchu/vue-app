0b2c1b17ec3ddc0acfe940e54583364ccae91c43
Erwin Maximiliano Schvindt <maximiliano.schvindt@wellsky.com>
2020-10-24 09:32:01 -0300

    Initial Load Method

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,12 +1,38 @@
 <template>
   <div>
-    <h1>Hey Beautiful World</h1>
+    <div v-if="loading">Loading..</div>
+    <div v-if="!loading && !error">Welcome</div>
+    <div v-if="!loading && error">Error!!</div>
   </div>
 </template>
 
 <script>
+import {mapActions} from "vuex";
+
 export default {
   name: "App",
+
+  data() {
+    return {
+      loading: true,
+      error: false
+    }
+  },
+
+  methods: {
+    ...mapActions(["initialLoad"])
+  },
+
+  async mounted() {
+    try {
+      await this.initialLoad();
+      this.loading = false;
+    } catch (error) {
+      this.loading = false;
+      this.error = true;
+
+    }
+  }
 };
 </script>
 
